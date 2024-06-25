24a0bcb907066ad1001a787152a8c8fc00759fe3
Vlad Miroshin <ppvlad@mail.ru>
2024-06-25 10:52:44 +0300

    fix (parser.js) create patronymic from male firstName

--- assets/js/parser.js
+++ assets/js/parser.js
@@ -2,7 +2,7 @@ import {PersonParts} from './classes.js';
 
 export async function parser (uri) {
 
-        let data;
+       let data;
 
        await fetch(uri)
         .then(response => response.json())
@@ -20,10 +20,43 @@ export async function parser (uri) {
         };
 
         parts.firstName_F = data.firstNameFemale;
-        parts.patronymic_M = data.patronymicMale;
-        parts.patronymic_F = data.patronymicFemale;
+
+        for (let i = 0; i < parts.firstName_M.length; i++) {
+            parts.patronymic_M.push(createPatronymicMale(parts.firstName_M[i]));
+        };
+        
+        for (let i = 0; i < parts.firstName_M.length; i++) {
+            parts.patronymic_F.push(createPatronymicFemale(parts.firstName_M[i]));
+        };
+
+        console.log(parts.patronymic_M);
+        console.log(parts.patronymic_F);
+
+        
         parts.profession_M = data.professionMale;
         parts.profession_F = data.professionFemale;
 
         return parts;
+}
+
+function createPatronymicMale(firstName) {
+    if (firstName.endsWith('й'))
+        return firstName.substr(0, firstName.length - 1) + 'евич';
+    else if (firstName.endsWith('слав'))
+        return firstName + 'ич';
+    else if (firstName.endsWith('а'))
+        return firstName.substr(0, firstName.length - 1) + 'ович';
+    else
+        return firstName + 'ович';
+}
+
+function createPatronymicFemale(firstName) {
+    if (firstName.endsWith('й'))
+        return firstName.substr(0, firstName.length - 1) + 'евна';
+    else if (firstName.endsWith('слав'))
+        return firstName + 'на';
+    else if (firstName.endsWith('а'))
+        return firstName.substr(0, firstName.length - 1) + 'овна';
+    else
+        return firstName + 'овна';
 }
